73312d4a59a59e97f42a5039f287a5ec42ab1def
Dor Lazarov <lazamal@gmail.com>
2025-11-09 19:51:26 +0200

    push to working branch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
-  providers: [provideRouter(routes)],
-
   declarations: [AppComponent],
+  imports: [BrowserModule],
+  providers: [provideRouter(routes)],
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
