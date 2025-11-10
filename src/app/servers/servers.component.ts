f4ba6d9a6a7fb71013cf0f2056384295548bdd32
Dor Lazarov <lazamal@gmail.com>
2025-11-10 22:26:33 +0200

    data binding 1

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -3,7 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: '[app-servers]',
   standalone: false,
-  template: `<app-server> </app-server> <app-server></app-server>`,
+  templateUrl: `./servers.component.html`,
   styleUrl: './servers.component.css',
 })
-export class ServersComponent {}
+export class ServersComponent {
+  allowNewServers = true;
+  newServerName: string = '';
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServers = false;
+    }, 2000);
+  }
+
+  onCreateServer() {
+    console.log('add server');
+  }
+
+  onChangeServerName(event) {
+    this.newServerName = event.target.value;
+  }
+}
